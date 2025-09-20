f1e3c3f8afb4c8e4ff8f2a5b23b51393027ee971
Ajinkya <sonarajinkya1@gmail.com>
2025-09-20 22:55:35 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,17 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.6.1/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.6.1/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.6.1/firebase-firestore.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD-Q-7DzSfnKLilGzJe20JoVcHcZmI2Xj4",
   authDomain: "grocery-manager-1ed63.firebaseapp.com",
   projectId: "grocery-manager-1ed63",
-  storageBucket: "grocery-manager-1ed63.appspot.com",  // <-- fixed
+  storageBucket: "grocery-manager-1ed63.appspot.com",
   messagingSenderId: "991897795361",
   appId: "1:991897795361:web:7e0200b1cdc078a4e51c60"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
